4b3cc61531eaf5cbc53db9007710f788d6ddbd2c
fweise <public@weise-games.de>
2024-02-05 20:23:41 +0100

    update license

--- tests/delegate.ts
+++ tests/delegate.ts
@@ -1,7 +1,7 @@
 /**
  * Aplenture/CoreJS
  * https://github.com/Aplenture/CoreJS
- * Copyright (c) 2023 Aplenture
+ * Copyright (c) 2024 Aplenture
  * License https://github.com/Aplenture/CoreJS/blob/main/LICENSE
  */
 
