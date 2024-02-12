0797c9742b774065294fef9910211182324842a4
fweise <public@weise-games.de>
2024-02-12 21:46:08 +0100

    clean up

--- src/core/routine.ts
+++ src/core/routine.ts
@@ -5,54 +5,64 @@
  * License https://github.com/Aplenture/CoreJS/blob/main/LICENSE
  */
 
-import { EVENT_ACTIVE_CHANGED, CACHE_INIT, EVENT_INIT } from "../constants";
-import { Module } from "./module";
+import { EVENT_ENABLED_CHANGED, CACHE_INIT, EVENT_INIT } from "../constants";
 import { Controller } from "./controller";
-import { EventHandler } from "./eventHandler";
+import { Handler } from "./handler";
+import { Delegatable, Delegate, DelegateCallback } from "./delegate";
 
-export class Routine extends EventHandler<Controller<any>> {
-    constructor(public readonly command: string) {
-        super({ event: EVENT_ACTIVE_CHANGED });
+export class Routine extends Handler<Controller<any>> {
+    private readonly _onExecute: Delegate<number>;
+
+    constructor(commandOrCallback: string | DelegateCallback<number>) {
+        super({ event: EVENT_ENABLED_CHANGED });
+
+        const callback = typeof commandOrCallback == "string"
+            ? () => this.emit(commandOrCallback)
+            : commandOrCallback;
+
+        this._onExecute = new Delegate(callback);
     }
 
+    public get onExecute(): Delegatable<number> { return this._onExecute; }
     public get running(): boolean { throw new Error('method not implemented'); }
 
-    public append(child: Module<Module<Controller<any>>>): void {
-        super.append(child);
-
-        // execute if app is already initialized
-        // otherwise execute on init event
-        if (this.get(CACHE_INIT))
-            this.execute();
-        else
-            this.parent.once(EVENT_INIT, () => this.execute());
-    }
-
-    public depend(child: Module<Module<Controller<any>>>): void {
-        super.depend(child);
-        this.stop();
-    }
-
-    protected async execute(): Promise<void> {
-        if (this.parent.active)
+    public async execute(): Promise<void> {
+        if (this.parent.enabled)
             this.start();
         else
             this.stop();
     }
 
-    protected start() {
+    public start(time = Date.now()) {
         if (this.running)
             return;
 
-        this.emit(this.command);
+        this._onExecute.invoke(time);
 
         throw new Error('method not implemented');
     }
 
-    protected stop() {
+    public stop() {
         if (!this.running)
             return;
 
         throw new Error('method not implemented');
+    }
+
+    protected onAppended(): void {
+        super.onAppended();
+
+        // execute if app is already initialized
+        // otherwise execute on init event
+        if (this.parent.get(CACHE_INIT))
+            this.execute();
+        else
+            this.parent.once(EVENT_INIT, () => this.execute());
+    }
+
+    protected onDepended(): void {
+        super.onDepended();
+
+        this.stop();
     }
 }
