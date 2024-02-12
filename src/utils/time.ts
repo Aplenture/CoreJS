cbea79177734b8ac4852ea55b650a09803efcd6e
fweise <public@weise-games.de>
2024-02-12 21:45:47 +0100

    implement date handling

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -5,6 +5,210 @@
  * License https://github.com/Aplenture/CoreJS/blob/main/LICENSE
  */
 
+/** Time units in milliseconds. */
+export enum Milliseconds {
+    Second = 1000,
+    Minute = 60 * Second,
+    Hour = 60 * Minute,
+    Day = 24 * Hour
+}
+
+/** 
+ * All week days parsed in numbers.
+ * Beginning with Sunday as 0.
+ */
+export enum WeekDay {
+    Sunday = 0,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+}
+
+/** 
+ * All months parsed in numbers.
+ * Beginning with Janary as 0.
+ */
+export enum Month {
+    January = 0,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
+
+/** Options to calculate a date. */
+export interface DateOpitons {
+    /** 
+     * The reference to calculate the date.
+     * Default is current date.
+     */
+    readonly date?: Date;
+
+    /** 
+     * Year of the day.
+     * Default is the year of the reference date.
+     */
+    readonly year?: number;
+
+    /** 
+     * Month of the day.
+     * Default is the month of the reference date.
+     */
+    readonly month?: Month;
+
+    /** 
+     * Day of the month between [1, 31].
+     * Default is the day of the reference date.
+     */
+    readonly monthDay?: number;
+
+    /** 
+     * Day of the week.
+     */
+    readonly weekDay?: WeekDay;
+
+    /** 
+     * Hours of the date. 
+     * Default is 0.
+     */
+    readonly hours?: number;
+
+    /** 
+     * Minutes of the date. 
+     * Default is 0.
+     */
+    readonly minutes?: number;
+
+    /** 
+     * Seconds of the date. 
+     * Default is 0.
+     */
+    readonly seconds?: number;
+
+    /** 
+     * Milliseconds of the date. 
+     * Default is 0.
+     */
+    readonly milliseconds?: number;
+}
+
+/** Options to add time to a date. */
+export interface AddOpitons {
+    /** 
+     * The reference to calculate the date.
+     * If not set, the reference is calculated by date().
+     */
+    readonly date?: Date;
+
+    /** 
+     * Number of years to add to the reference. 
+     * Default is 0.
+     */
+    readonly years?: number;
+
+    /** 
+     * Number of months to add to the reference. 
+     * Default is 0.
+     */
+    readonly months?: number;
+
+    /** 
+     * Number of days to add to the reference. 
+     * Default is 0.
+     */
+    readonly days?: number;
+
+    /** 
+     * Number of hours to add to the reference. 
+     * Default is 0.
+     */
+    readonly hours?: number;
+
+    /** 
+     * Number of minutes to add to the reference. 
+     * Default is 0.
+     */
+    readonly minutes?: number;
+
+    /** 
+     * Number of seconds to add to the reference. 
+     * Default is 0.
+     */
+    readonly seconds?: number;
+
+    /** 
+     * Number of milliseconds to add to the reference. 
+     * Default is 0.
+     */
+    readonly milliseconds?: number;
+}
+
+/** Calculates local date by opitons. */
+export function date(options: DateOpitons = {}) {
+    const tmp = options.date || new Date();
+
+    const result = new Date(
+        options.year ?? tmp.getFullYear(),
+        options.month ?? tmp.getMonth(),
+        options.monthDay ?? tmp.getDate(),
+        options.hours || 0,
+        options.minutes || 0,
+        options.seconds || 0,
+        options.milliseconds || 0
+    );
+
+    if (undefined != options.weekDay) {
+        const weekDay: WeekDay = result.getDay();
+
+        // calculate day of month by week day
+        // first calculate day of last monday
+        // then add number of days to calculate the target week day
+        result.setDate(result.getDate() - weekDay + options.weekDay);
+    }
+
+    return result;
+}
+
+/** Increases a local date by opitons. */
+export function add(options: AddOpitons = {}) {
+    const tmp = options.date || date();
+
+    return new Date(
+        tmp.getFullYear() + (options.years || 0),
+        tmp.getMonth() + (options.months || 0),
+        tmp.getDate() + (options.days || 0),
+        tmp.getHours() + (options.hours || 0),
+        tmp.getMinutes() + (options.minutes || 0),
+        tmp.getSeconds() + (options.seconds || 0),
+        tmp.getMilliseconds() + (options.milliseconds || 0)
+    );
+}
+
+/** Decreases a local date by opitons. */
+export function reduce(options: AddOpitons = {}) {
+    const tmp = options.date || date();
+
+    return new Date(
+        tmp.getFullYear() - (options.years || 0),
+        tmp.getMonth() - (options.months || 0),
+        tmp.getDate() - (options.days || 0),
+        tmp.getHours() - (options.hours || 0),
+        tmp.getMinutes() - (options.minutes || 0),
+        tmp.getSeconds() - (options.seconds || 0),
+        tmp.getMilliseconds() - (options.milliseconds || 0)
+    );
+}
+
 /**
  * Parses date to string with specific format.
  * Example 1707136662666 to '2024-02-05 13:37:42.666'.
--- tests/time.ts
+++ tests/time.ts
@@ -34,4 +34,377 @@ describe("Time", () => {
             expect(format).equals("2024-02-05 13:37:42");
         });
     });
+
+    describe("date()", () => {
+        describe("date", () => {
+            it("calculates start of day", () => {
+                const reference = new Date();
+                const date = Time.date();
+
+                expect(date.getFullYear()).equals(reference.getFullYear(), "year");
+                expect(date.getMonth()).equals(reference.getMonth(), "month");
+                expect(date.getDate()).equals(reference.getDate(), "day");
+                expect(date.getHours()).equals(0, "hours");
+                expect(date.getMinutes()).equals(0, "minutes");
+                expect(date.getSeconds()).equals(0, "seconds");
+                expect(date.getMilliseconds()).equals(0, "milliseconds");
+            });
+
+            it("considers a different date reference", () => {
+                const date = new Date("2024-02-11");
+                const target = new Date("2024-02-11T00:00:00.000");
+
+                expect(Time.date({ date })).deep.equals(target);
+            });
+        });
+
+        describe("year", () => {
+            it("changes the year", () => {
+                const date = new Date("2024-02-12");
+                const target = new Date("2023-02-12T00:00:00.000");
+
+                expect(Time.date({ date, year: 2023 })).deep.equals(target);
+            });
+        });
+
+        describe("month", () => {
+            it("changes the month", () => {
+                const date = new Date("2024-02-12");
+                const target = new Date("2024-01-12T00:00:00.000");
+
+                expect(Time.date({ date, month: Time.Month.January })).deep.equals(target);
+            });
+        });
+
+        describe("day", () => {
+            it("changes to first day of febrary", () => {
+                const date = new Date("2024-02-12");
+                const target = new Date("2024-02-01T00:00:00.000");
+
+                expect(Time.date({ date, monthDay: 1 })).deep.equals(target);
+            });
+
+            it("changes to last day of long febrary", () => {
+                const date = new Date("2024-02-12");
+                const target = new Date("2024-02-29T00:00:00.000");
+
+                expect(Time.date({ date, monthDay: 29 })).deep.equals(target);
+            });
+
+            it("overflows to first day of march after short febrary", () => {
+                const date = new Date("2025-02-12");
+                const target = new Date("2025-03-01T00:00:00.000");
+
+                expect(Time.date({ date, monthDay: 29 })).deep.equals(target);
+            });
+        });
+
+        describe("week day", () => {
+            it("changes from monday to saturday", () => {
+                const date = new Date("2024-02-12");
+                const target = new Date("2024-02-17T00:00:00.000");
+
+                expect(Time.date({ date, weekDay: Time.WeekDay.Saturday })).deep.equals(target);
+            });
+
+            it("changes from monday to sunday", () => {
+                const date = new Date("2024-02-12");
+                const target = new Date("2024-02-11T00:00:00.000");
+
+                expect(Time.date({ date, weekDay: Time.WeekDay.Sunday })).deep.equals(target);
+            });
+
+            it("changes from sunday to monday", () => {
+                const date = new Date("2024-02-11");
+                const target = new Date("2024-02-12T00:00:00.000");
+
+                expect(Time.date({ date, weekDay: Time.WeekDay.Monday })).deep.equals(target);
+            });
+
+            it("changes from sunday to saturday", () => {
+                const date = new Date("2024-02-11");
+                const target = new Date("2024-02-17T00:00:00.000");
+
+                expect(Time.date({ date, weekDay: Time.WeekDay.Saturday })).deep.equals(target);
+            });
+        });
+
+        describe("hours", () => {
+            it("changes hours", () => {
+                const date = new Date("2024-02-12");
+                const target = new Date("2024-02-12T01:00:00.000");
+
+                expect(Time.date({ date, hours: 1 })).deep.equals(target);
+            });
+
+            it("overflows hours to days", () => {
+                const date = new Date("2024-02-12");
+                const target = new Date("2024-02-13T01:00:00.000");
+
+                expect(Time.date({ date, hours: 25 })).deep.equals(target);
+            });
+        });
+
+        describe("minutes", () => {
+            it("changes minutes", () => {
+                const date = new Date("2024-02-12");
+                const target = new Date("2024-02-12T00:01:00.000");
+
+                expect(Time.date({ date, minutes: 1 })).deep.equals(target);
+            });
+
+            it("overflows minutes to hours", () => {
+                const date = new Date("2024-02-12");
+                const target = new Date("2024-02-12T01:01:00.000");
+
+                expect(Time.date({ date, minutes: 61 })).deep.equals(target);
+            });
+        });
+
+        describe("seconds", () => {
+            it("changes seconds", () => {
+                const date = new Date("2024-02-12");
+                const target = new Date("2024-02-12T00:00:01.000");
+
+                expect(Time.date({ date, seconds: 1 })).deep.equals(target);
+            });
+
+            it("overflows seconds to minutes", () => {
+                const date = new Date("2024-02-12");
+                const target = new Date("2024-02-12T00:01:01.000");
+
+                expect(Time.date({ date, seconds: 61 })).deep.equals(target);
+            });
+        });
+
+        describe("milliseconds", () => {
+            it("changes milliseconds", () => {
+                const date = new Date("2024-02-12");
+                const target = new Date("2024-02-12T00:00:00.001");
+
+                expect(Time.date({ date, milliseconds: 1 })).deep.equals(target);
+            });
+
+            it("overflows milliseconds to seconds", () => {
+                const date = new Date("2024-02-12");
+                const target = new Date("2024-02-12T00:00:01.001");
+
+                expect(Time.date({ date, milliseconds: 1001 })).deep.equals(target);
+            });
+        });
+    });
+
+    describe("add()", () => {
+        describe("years", () => {
+            it("adds the year", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2025-02-12T00:00:00.000");
+
+                expect(Time.add({ date, years: 1 })).deep.equals(target);
+            });
+        });
+
+        describe("months", () => {
+            it("adds the month", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2024-03-12T00:00:00.000");
+
+                expect(Time.add({ date, months: 1 })).deep.equals(target);
+            });
+
+            it("overflows to year", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2025-01-12T00:00:00.000");
+
+                expect(Time.add({ date, months: 11 })).deep.equals(target);
+            });
+        });
+
+        describe("days", () => {
+            it("adds the day", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2024-02-13T00:00:00.000");
+
+                expect(Time.add({ date, days: 1 })).deep.equals(target);
+            });
+
+            it("overflows to month", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2024-03-01T00:00:00.000");
+
+                expect(Time.add({ date, days: 18 })).deep.equals(target);
+            });
+        });
+
+        describe("hours", () => {
+            it("adds the hours", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2024-02-12T01:00:00.000");
+
+                expect(Time.add({ date, hours: 1 })).deep.equals(target);
+            });
+
+            it("overflows to day", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2024-02-13T00:00:00.000");
+
+                expect(Time.add({ date, hours: 24 })).deep.equals(target);
+            });
+        });
+
+        describe("minutes", () => {
+            it("adds the minutes", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2024-02-12T00:01:00.000");
+
+                expect(Time.add({ date, minutes: 1 })).deep.equals(target);
+            });
+
+            it("overflows to hours", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2024-02-12T01:00:00.000");
+
+                expect(Time.add({ date, minutes: 60 })).deep.equals(target);
+            });
+        });
+
+        describe("seconds", () => {
+            it("adds the seconds", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2024-02-12T00:00:01.000");
+
+                expect(Time.add({ date, seconds: 1 })).deep.equals(target);
+            });
+
+            it("overflows to minutes", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2024-02-12T00:01:00.000");
+
+                expect(Time.add({ date, seconds: 60 })).deep.equals(target);
+            });
+        });
+
+        describe("milliseconds", () => {
+            it("adds the milliseconds", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2024-02-12T00:00:00.001");
+
+                expect(Time.add({ date, milliseconds: 1 })).deep.equals(target);
+            });
+
+            it("overflows to seconds", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2024-02-12T00:00:01.000");
+
+                expect(Time.add({ date, milliseconds: 1000 })).deep.equals(target);
+            });
+        });
+    });
+
+    describe("reduce()", () => {
+        describe("years", () => {
+            it("reduces the year", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2023-02-12T00:00:00.000");
+
+                expect(Time.reduce({ date, years: 1 })).deep.equals(target);
+            });
+        });
+
+        describe("months", () => {
+            it("reduces the month", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2024-01-12T00:00:00.000");
+
+                expect(Time.reduce({ date, months: 1 })).deep.equals(target);
+            });
+
+            it("overflows to year", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2023-12-12T00:00:00.000");
+
+                expect(Time.reduce({ date, months: 2 })).deep.equals(target);
+            });
+        });
+
+        describe("days", () => {
+            it("reduces the day", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2024-02-11T00:00:00.000");
+
+                expect(Time.reduce({ date, days: 1 })).deep.equals(target);
+            });
+
+            it("overflows to month", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2024-01-31T00:00:00.000");
+
+                expect(Time.reduce({ date, days: 12 })).deep.equals(target);
+            });
+        });
+
+        describe("hours", () => {
+            it("reduces the hours", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12, hours: 1 });
+                const target = new Date("2024-02-12T00:00:00.000");
+
+                expect(Time.reduce({ date, hours: 1 })).deep.equals(target);
+            });
+
+            it("overflows to day", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2024-02-11T23:00:00.000");
+
+                expect(Time.reduce({ date, hours: 1 })).deep.equals(target);
+            });
+        });
+
+        describe("minutes", () => {
+            it("reduces the minutes", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12, minutes: 1 });
+                const target = new Date("2024-02-12T00:00:00.000");
+
+                expect(Time.reduce({ date, minutes: 1 })).deep.equals(target);
+            });
+
+            it("overflows to hours", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2024-02-11T23:59:00.000");
+
+                expect(Time.reduce({ date, minutes: 1 })).deep.equals(target);
+            });
+        });
+
+        describe("seconds", () => {
+            it("reduces the seconds", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12, seconds: 1 });
+                const target = new Date("2024-02-12T00:00:00.000");
+
+                expect(Time.reduce({ date, seconds: 1 })).deep.equals(target);
+            });
+
+            it("overflows to minutes", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2024-02-11T23:59:59.000");
+
+                expect(Time.reduce({ date, seconds: 1 })).deep.equals(target);
+            });
+        });
+
+        describe("milliseconds", () => {
+            it("reduces the milliseconds", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12, milliseconds: 1 });
+                const target = new Date("2024-02-12T00:00:00.000");
+
+                expect(Time.reduce({ date, milliseconds: 1 })).deep.equals(target);
+            });
+
+            it("overflows to seconds", () => {
+                const date = Time.date({ year: 2024, month: Time.Month.February, monthDay: 12 });
+                const target = new Date("2024-02-11T23:59:59.999");
+
+                expect(Time.reduce({ date, milliseconds: 1 })).deep.equals(target);
+            });
+        });
+    });
 });
